a4070efe45d2a8ed00d02aff333b6b199ddda54d
Srujan <srujank@boston-technology.com>
2020-01-24 17:31:05 +0530

    Update App.js
    Commit2

--- TextInput/App.js
+++ TextInput/App.js
@@ -4,6 +4,7 @@ import PlaceList from './src/components/PlaceList/PlaceList';
 import PlaceInput from './src/components/PlaceInput/PlaceInput';
 
 //Changed Govind
+//Srujan
 export default class App extends React.Component {
 
   state = {
